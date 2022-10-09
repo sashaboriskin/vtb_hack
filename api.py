5f99d14d3fd10da99d5beebdb4e250bcda5ed45e
sashaboriskin <brskn.lx@gmail.com>
2022-10-09 09:47:39 +0300

    change method of ranking

--- api.py
+++ api.py
@@ -1,8 +1,25 @@
 import pandas as pd
+pd.options.mode.chained_assignment = None
+
+
+def get_all_seconds(year: int, month: int, day: int, secs: int) -> int:
+  return (year*12*30*24*60*60 + month*30*24*60*60 + day*60*60 + secs)
 
 
 def recommend_news(df: pd.DataFrame, role: str) -> list:
-    df = df.sort_values(by=['views'], ascending=False)
+    df['jesos_seconds'] = 0
+    for i in range(len(df)):
+        year = df['year'].iloc[[i]]
+        month = df['month'].iloc[[i]]
+        day = df['day'].iloc[[i]]
+        secs = df['seconds'].iloc[[i]]
+        df['jesos_seconds'].iloc[[i]] = get_all_seconds(year, month, day, secs)    
+    mean_jesos_seconds = df['jesos_seconds'].mean()
+    mean_views = df['views'].mean()
+    df['recom_coof'] = 0
+    for i in range(len(df)):
+        df['recom_coof'].iloc[[i]] = (df['views'].iloc[[i]]/mean_views)/(df['jesos_seconds'].iloc[[i]]/mean_jesos_seconds)    
+    df = df.sort_values(by=['recom_coof'], ascending=False)
     if role == 'it': 
         best_news = df.loc[df['rubrics'] == 1].iloc[:3]
     else:
@@ -10,8 +27,8 @@ def recommend_news(df: pd.DataFrame, role: str) -> list:
     recomendations = []
     for i in range(3):
         recomendations.append(best_news['link'].iloc[[i]].values[0])
-    return recomendations 
-          
+    return recomendations       
+
 
 habr_df = pd.read_csv("C:\\Users\\sasha\\PycharmProjects\\vtb_hack2\\data\\habr_recomendations.csv", index_col=0)
 role = input("Input the business role: ")
